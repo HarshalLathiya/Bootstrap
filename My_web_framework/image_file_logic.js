03448b57272d998577b3adceac52dfbbad28e8b7
HarshalLathiya <harshalplathiya@gmail.com>
2025-06-14 11:52:52 +0530

    new update

--- My_web_framework/image_file_logic.js
+++ My_web_framework/image_file_logic.js
@@ -1,27 +1,54 @@
-window.onload = function() {
+window.onload = function () {
   const modal = document.querySelector(".modal");
   const modalImage = document.querySelector(".modal img");
   const closeButton = document.querySelector(".close");
 
-  function openModal(src) {
+  function openModal(event, src) {
+    const clickedImage = event.target;
+
+    if (clickedImage.classList.contains("premium-image")) {
+      alert("This is a premium image. Please select a plan to unlock premium images.");
+    
+      const pricingSection = document.getElementById("pricing");
+    
+      if (pricingSection) {
+        pricingSection.scrollIntoView({ behavior: "smooth" });
+      } else {
+        // If not on this page, redirect to pricing on homepage
+        window.location.href = "my_website.html#pricing"; 
+      }
+      return;
+    }
+    
     modalImage.src = src;
     modal.classList.add("show");
   }
 
   function closeModal() {
     modal.classList.remove("show");
+    modalImage.src = ""; 
   }
 
-  modal.addEventListener("click", function(e) {
+  // Click outside the image to close modal
+  modal.addEventListener("click", function (e) {
     if (e.target.classList.contains("modal")) {
       closeModal();
     }
   });
 
-  closeButton.addEventListener("click", function() {
+  // Close button click handler
+  closeButton.addEventListener("click", function () {
     closeModal();
   });
 
+  // Optional: allow ESC key to close the modal
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape") {
+      closeModal();
+    }
+  });
+
+  // Make functions globally accessible to HTML
   window.openModal = openModal;
   window.closeModal = closeModal;
 };
